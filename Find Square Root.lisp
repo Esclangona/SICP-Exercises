804fe40e3875a5a649e5cf62ff06952553dd69c4
Esclangona <kubilay.bilgili@gmail.com>
2020-10-11 20:33:04 +0300

    Create Find Square Root.lisp

---
+++ Find Square Root.lisp
@@ -0,0 +1,27 @@
+;Square Roots by Newton's Method 
+
+;returns avarage of given numbers
+(define (avarage x y) (/(+ x y) 2))
+
+;returns given number squared
+(define (square x) (* x x) )
+
+;improves the guess by avareging the guess with x/guess
+(define (improve guess x)
+	(avarage guess (/ x guess)))
+
+;returns true if absolute value of guess squared - x(radicand) is smaller than the argument
+(define (good_enough guess x)
+		(< (abs(- (square guess) x)) 0.000000001))
+
+;given two arguments returns guess as good enough approximation
+(define (sqits guess x)
+	(if (good_enough guess x)
+		guess
+		(sqits (improve guess x) x )
+	)
+)
+
+;returns sqrt of a number
+(define (sqrt x)
+	(sqits 1.0 x))
